9812b9c6dfa2fec64d6fde57ed693a20d0c8599c
Wisdom <152782718+WolfieWTF@users.noreply.github.com>
2024-02-14 16:39:26 +0100

    Update extra.js

--- src/extra.js
+++ src/extra.js
@@ -23,7 +23,7 @@ cmd({
     },
     async(Void, citel) => {
         if(!citel.quoted) return citel.reply('Please quote any text to get link.')
-        let data = await pastebin.createPaste(citel.quoted.text, "XLICON-Pastebin")
+        let data = await pastebin.createPaste(citel.quoted.text, "𝚆𝙾𝙻𝙵𝙸𝙴-Pastebin")
         citel.reply('_Here is your link._\n'+data)
     }
 );
