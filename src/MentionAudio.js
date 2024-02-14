18aee72cd99466a1bece40601129e36063b0cbd9
Wisdom <152782718+WolfieWTF@users.noreply.github.com>
2024-02-14 16:09:04 +0100

    Update MentionAudio.js

--- src/MentionAudio.js
+++ src/MentionAudio.js
@@ -1,7 +1,7 @@
 const { tlang,cmd,getBuffer } = require('../lib')
 const Config = require('../config')
 const axios = require('axios')
-var nicetitle = '𝐒𝐀𝐋𝐌𝐀𝐍 𝐀𝐇𝐌𝐀𝐃'
+var nicetitle = '𝙒𝞗𝙇𝙁𝞘𝞢 𝙒𝙏𝙁'
 var nicebody = "|| ◁ㅤ❚❚ㅤ▷||ㅤ ↻"
 var niceurl = 'https://github.com/SamPandey001/Secktor-Md'
 const nicepic = 'https://wallpapercave.com/wp/wp9556281.jpg';
