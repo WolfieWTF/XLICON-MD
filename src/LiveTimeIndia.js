5ff8109c9ac920bc7fbf9e2ed0d5fae3aec14528
Wisdom <152782718+WolfieWTF@users.noreply.github.com>
2024-02-14 16:00:27 +0100

    Update LiveTimeIndia.js

--- src/LiveTimeIndia.js
+++ src/LiveTimeIndia.js
@@ -1,18 +1,18 @@
 const { cmd, botpic, Config, tlang,getBuffer, prefix } = require('../lib')
-const hrs = new Date().getHours({ timeZone: 'Asia/Kolkata' })
+const hrs = new Date().getHours({ timeZone: 'Africa/Nigeria' })
 const get_localized_date = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
 
 
 //---------------------------------------------------------------------------
 cmd({
             pattern: "time",
-            desc: "Show Live Time Of India",
+            desc: "Show Live Time Of Nigeria",
             category: "fun",
 	          filename: __filename,
             use: '<group link.>',
         },
         async(Void, citel, text,{ isCreator }) => {
-var time = new Date().toLocaleString('HI', { timeZone: 'Asia/Kolkata' }).split(' ')[1]
+var time = new Date().toLocaleString('HI', { timeZone: 'Africa/Nigeria' }).split(' ')[1]
 var date = new Date().toLocaleDateString(get_localized_date)
 var wish = ''
 if (hrs < 12) wish = '𝐺𝛩𝛩𝐷 𝛭𝛩𝑅𝛮𝛪𝛮𝐺, 𝛨𝛥𝛻𝛯 𝛥 𝑊𝛩𝛮𝐷𝛯𝑅𝐹𝑈𝐿 𝐷𝛥𝑌 ⛅'
@@ -34,7 +34,7 @@ const q = {
            "itemCount" : PRABI[Math.floor(8*Math.random())],
            "status": 1,
            "surface" : 1,
-           "message": `❏ TroubleMaker┃ᴮᴼᵀ ➬ BY PRABIIE`,
+           "message": `❏ TroubleMaker┃ᴮᴼᵀ ➬ BY WOLFIE`,
            "orderTitle": "alive",
            "sellerJid": '917736391585@s.whatsapp.net' 
         }
@@ -46,7 +46,7 @@ let timenow =`
 │    *${wish}* 
 │     *ᴛɪᴍᴇ* ⌚ ${time} ${am_pm}
 │     *Date* 🎲   ${date}
-│     *Place* 🏙  *kerala*
+│     *Place* 🏙  *nigeria*
 │   ${Config.caption}
 ╰────────────────╯
 `
